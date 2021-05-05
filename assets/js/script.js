c6566139c08afcaf0bc742e1971da4e3febe37cc
Steve C <stevencarofano@gmail.com>
2021-05-05 13:16:53 -0400

    Combining first and second functions, tweaked HTML and added favicon

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,34 +19,38 @@ function getApi() {
         characterDisplay.textContent = "Character: " + data.character;
         quoteDisplay.textContent = "Quote: " + data.quote;
 
+        var gifUrl = 'https://api.giphy.com/v1/gifs/search?api_key=yCzBacG75tZ5SWin4tVnxzMtl3uF3WIe&limit=1&q=' + data.character;
+        fetch(gifUrl)
+        .then(function(response) {
+        return response.json();
+        })
+        
+        // From Steve Griffith video, creating HTML elements to show the generated gif
+        .then(function(content) {
+        console.log(content.data);
+        // Creating HTML elements to display generated gif
+        var fig = document.createElement('figure');
+        var img = document.createElement('img');
+        var fc = document.createElement('figcaption');
+        // Setting image source, alt text and caption
+        img.src = content.data[0].images.downsized.url;
+        img.alt = content.data[0].title;
+        fc.textContent = content.data[0].title;
+        // Appending image and caption
+        fig.appendChild(img);
+        fig.appendChild(fc);
+        var gifDiv = document.getElementById('gifDiv');
+        // Inserting generated gif at top of container
+        gifDiv.insertAdjacentElement('afterbegin', fig);
+        })
     })
 
 }
 // Gif function 
-function init(){
-            var gifUrl = 'https://api.giphy.com/v1/gifs/random?api_key=yCzBacG75tZ5SWin4tVnxzMtl3uF3WIe&limit=1';
-        fetch(gifUrl)
-            .then(function(response) {
-            return response.json();
-            })
-        
-        // From Steve Griffith video, creating HTML elements to show the generated gif
-            .then(function(content) {
-            console.log(content.data);
-            var fig = document.createElement('figure');
-            var img = document.createElement('img');
-            var fc = document.createElement('figcaption');
-            img.src = content.data.images.downsized.url;
-            img.alt = content.data.title;
-            fc.textContent = content.data.title;
-            fig.appendChild(img);
-            fig.appendChild(fc);
-            var gifDiv = document.getElementById('gifDiv');
-            gifDiv.insertAdjacentElement('afterbegin', fig);
-        })
-    
 
-}
+// function init(){
 
-picBtn.addEventListener('click', init);
+
+
+// picBtn.addEventListener('click', init);
 fetchButton.addEventListener('click', getApi);
